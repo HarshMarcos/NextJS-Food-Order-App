59c094544aaee743c1b16dc26dc7242885225935
admin <harshkurulkar7@gmail.com>
2024-01-11 16:12:49 +0530

    initial commit

---
+++ src/components/layouts/HomeMenu.jsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import React from "react";
+
+const HomeMenu = () => {
+  return (
+    // <section>
+    //   <div>
+    //     <Image src={"/sallad1.png"} width={100} height={100} alt="Salad" />
+    //   </div>
+    //   <div className="text-center">
+    //     <h3 className="uppercase text-gray-500 font-semibold leading-4">
+    //       Check Out
+    //     </h3>
+    //     <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
+    //   </div>
+    // </section>
+    <section className="">
+      <div className="absolute left-0 right-0 w-full justify-start">
+        <div className="absolute left-0 -top-[70px] text-left -z-10">
+          <Image src={"/sallad1.png"} width={109} height={189} alt={"sallad"} />
+        </div>
+        <div className="absolute -top-[100px] right-0 -z-10">
+          <Image src={"/sallad2.png"} width={107} height={195} alt={"sallad"} />
+        </div>
+      </div>
+      <div className="text-center mb-4">
+        <h3 className="uppercase text-gray-500 font-semibold leading-4">
+          Check Out
+        </h3>
+        <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
+      </div>
+      {/* <div className="grid sm:grid-cols-3 gap-4">
+        {bestSellers?.length > 0 && bestSellers.map(item => (
+          <MenuItem key={item._id} {...item} />
+        ))}
+      </div> */}
+    </section>
+  );
+};
+
+export default HomeMenu;
